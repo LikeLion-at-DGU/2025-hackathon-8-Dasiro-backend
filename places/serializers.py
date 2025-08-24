13ac7b664f2b7b81de03be789832dcdd34204566
lsoobin <vini20012203@gmail.com>
2025-08-25 03:39:42 +0900

    fix: 회복상권 카테고리별 대표 이미지 설정

--- places/serializers.py
+++ places/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from .models import Place
 
 CATEGORY_IMAGES = {
-    'FOOD': 'https://dasirobucket.s3.ap-northeast-2.amazonaws.com/food.png',
-    'CAFE': 'https://dasirobucket.s3.ap-northeast-2.amazonaws.com/cafe.png',
-    'CONVENIENCE': 'https://dasirobucket.s3.ap-northeast-2.amazonaws.com/convenience.png',
+    'FOOD': 'https://dasirobucket.s3.ap-northeast-2.amazonaws.com/Property+1%3D%EC%9D%8C%EC%8B%9D%EC%A0%90.png',
+    'CAFE': 'https://dasirobucket.s3.ap-northeast-2.amazonaws.com/Property+1%3D%EC%B9%B4%ED%8E%98.png',
+    'CONVENIENCE': 'https://dasirobucket.s3.ap-northeast-2.amazonaws.com/Property+1%3D%ED%8E%B8%EC%9D%98%EC%A0%90.png',
 }
 
 class PlaceSerializer(serializers.ModelSerializer):
