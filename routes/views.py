2bd1cd0890ae34dee416c4363a872ef69ea680d1
soobinLIM <163871529+lsoobin@users.noreply.github.com>
2025-08-25 12:11:47 +0900

    Update views.py

--- routes/views.py
+++ routes/views.py
@@ -198,8 +198,10 @@ class ORSProxyViewSet(viewsets.ViewSet):
         avoid_polygons = None
         if avoid_incidents:
             incidents = RecoveryIncident.objects.filter(status__in=avoid_status)
-            polygons = [make_circle_polygon(float(inc.lat), float(inc.lng), avoid_radius_m)
-                        for inc in incidents]
+            polygons = [
+                make_circle_polygon(float(inc.lat), float(inc.lng), avoid_radius_m)
+                for inc in incidents
+            ]
             if polygons:
                 avoid_polygons = {
                     "type": "MultiPolygon",
@@ -211,8 +213,7 @@ class ORSProxyViewSet(viewsets.ViewSet):
                 [float(origin["lng"]), float(origin["lat"])],
                 [float(destination["lng"]), float(destination["lat"])]
             ],
-            "elevation": True,
-            "instructions": False
+            "elevation": True
         }
         if avoid_polygons:
             body["avoid_polygons"] = avoid_polygons
@@ -232,13 +233,9 @@ class ORSProxyViewSet(viewsets.ViewSet):
 
             feature = data["features"][0]
             summary = feature.get("properties", {}).get("summary", {})
-            geometry_data = feature.get("geometry", {})
+            coords = feature.get("geometry", {}).get("coordinates", [])
 
-            polyline = []
-            for coord in geometry_data.get("coordinates", []):
-                if len(coord) >= 2:
-                    lng, lat = coord[0], coord[1]
-                    polyline.append([lat, lng])
+            polyline = [[lat, lng] for lng, lat, *_ in coords if len(coords) >= 2]
 
             RouteLog.objects.create(
                 origin_lat=float(origin["lat"]),
